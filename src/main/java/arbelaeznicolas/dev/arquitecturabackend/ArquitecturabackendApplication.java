f195e833c3e273617fcd15e9cb5d6e34df029cdf
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-02-09 16:14:22 -0500

    Add initial configuration of Spring Boot project

---
+++ src/main/java/arbelaeznicolas/dev/arquitecturabackend/ArquitecturabackendApplication.java
@@ -0,0 +1,13 @@
+package arbelaeznicolas.dev.arquitecturabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arbelaeznicolas/dev/arquitecturabackend/ArquitecturabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package arbelaeznicolas.dev.arquitecturabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
